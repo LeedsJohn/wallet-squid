2eec1013aacc4fbfb8e62a9f6b986b8566c7b266
John Leeds <leedsjohn6@gmail.com>
2025-04-28 15:08:43 -0700

    add commands to add and remove tag connections #1

--- lib/tag_commands.ml
+++ lib/tag_commands.ml
@@ -7,6 +7,42 @@ let list_tags =
      fun () -> Tag_info.print_sorted_freq_list tag_info)
 ;;
 
+let add_connection =
+  Command.basic
+    ~summary:"Add a connection from one tag to another"
+    ~readme:(fun () ->
+      "If you add a connection from tag x to tag y, then every note tagged with x will \
+       also be tagged with y.")
+    (let%map_open.Command base_path = Base_path.param
+     and from =
+       flag "-from" (required Tag.arg_type) ~doc:"tag Tag to add a connection from"
+     and to_ = flag "-to" (required Tag.arg_type) ~doc:"tag Tag to add a connection to" in
+     fun () ->
+       let open Tag_dag in
+       let tag_dag = load base_path |> add_edge ~from ~to_ |> ok_exn in
+       save tag_dag base_path)
+;;
+
+let remove_connection =
+  Command.basic
+    ~summary:"Remove a connection from one tag to another"
+    (let%map_open.Command base_path = Base_path.param
+     and from =
+       flag "-from" (required Tag.arg_type) ~doc:"tag Tag to remove a connection from"
+     and to_ =
+       flag "-to" (required Tag.arg_type) ~doc:"tag Tag to remove a connection to"
+     in
+     fun () ->
+       let open Tag_dag in
+       let tag_dag = load base_path |> remove_edge ~from ~to_ in
+       save tag_dag base_path)
+;;
+
 let command =
-  Command.group ~summary:"commands to interact with tags" [ "list", list_tags ]
+  Command.group
+    ~summary:"commands to interact with tags"
+    [ "list", list_tags
+    ; "add-connection", add_connection
+    ; "remove-connection", remove_connection
+    ]
 ;;
