0e67e4461ff6b07f6c582267d25be264fe90d65d
John Leeds <leedsjohn6@gmail.com>
2025-04-29 11:36:17 -0700

    Change base path to read environment variable if flag not provided

--- lib/base_path.ml
+++ lib/base_path.ml
@@ -1,5 +1,7 @@
 open! Core
 
+let environment_variable_name = "WALLET_SQUID_BASE_PATH"
+
 type t = Filename.t
 
 let of_string path =
@@ -7,6 +9,25 @@ let of_string path =
   | `Yes -> Ok path
   | `No | `Unknown ->
     error_s [%message "provided path is not a directory" (path : string)]
+;;
+
+let load_environment_variable () =
+  match Sys.getenv environment_variable_name with
+  | None ->
+    error_s
+      [%message
+        "environment variable does not exist"
+          ~environment_variable:(environment_variable_name : string)]
+  | Some s ->
+    (match of_string s with
+     | Ok t -> Ok t
+     | Error _ as e ->
+       Or_error.tag_s
+         e
+         ~tag:
+           [%message
+             "got value from environment variable"
+               ~environment_variable:(environment_variable_name : string)])
 ;;
 
 let to_filename t = t
@@ -17,8 +38,20 @@ let param =
   let%map_open.Command base_path =
     flag
       "-base-path"
-      (required arg_type)
-      ~doc:"path Path to root of directory containing notes"
+      (optional arg_type)
+      ~doc:
+        [%string
+          "path Path to root of directory containing notes. If it is not provided, it \
+           will attempt to read the environment variable %{environment_variable_name}."]
   in
-  base_path
+  match base_path with
+  | Some res -> res
+  | None ->
+    (match load_environment_variable () with
+     | Ok _ as res -> res
+     | Error _ as e ->
+       Or_error.tag_s
+         e
+         ~tag:[%message "no base path provided, reading environment variable"])
+    |> ok_exn
 ;;
