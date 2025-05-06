b3a99de304cf7d6b58c10559404976b52d99ec8e
John Leeds <leedsjohn6@gmail.com>
2025-05-06 12:49:59 -0700

    add bare minimum dot language printer

---
+++ lib/dot_writer.ml
@@ -0,0 +1,32 @@
+open! Core
+
+let make_dot ~nodes ~edges =
+  let nodes = Set.to_list nodes |> List.sort ~compare:String.compare in
+  let edges =
+    List.dedup_and_sort edges ~compare:(fun (s11, s12) (s21, s22) ->
+      let open String in
+      if s11 = s21 then compare s12 s22 else compare s11 s21)
+  in
+  print_endline "digraph G {";
+  List.iter nodes ~f:(fun n -> print_endline (n ^ ";"));
+  List.iter edges ~f:(fun (from, to_) -> print_endline [%string "%{from} -> %{to_};"]);
+  print_endline "}"
+;;
+
+let%expect_test "simple dot" =
+  let nodes = Set.of_list (module String) [ "a"; "b"; "c"; "d" ] in
+  let edges = [ "a", "b"; "a", "c"; "c", "d"; "c", "d" ] in
+  make_dot ~nodes ~edges;
+  [%expect
+    {|
+    digraph G {
+    a;
+    b;
+    c;
+    d;
+    a -> b;
+    a -> c;
+    c -> d;
+    }
+    |}]
+;;
---
+++ lib/dot_writer.mli
@@ -0,0 +1,13 @@
+open! Core
+
+(** Bare minimum dot language printer. Example usage:
+
+    [make_dot ~nodes:(Set.of_list ["a", "b"]) ~edges:[("a", "b")]]
+
+    Output:
+    digraph G {
+        a;
+        b;
+        a -> b;
+    } *)
+val make_dot : nodes:Set.M(String).t -> edges:(string * string) list -> unit
